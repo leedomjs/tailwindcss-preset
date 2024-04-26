8f29d7893a072b6188973e8a43cfc5eb5732c9f9
Leedom <jld1221@163.com>
2024-04-26 22:09:25 +0800

    fix(color): remove wrong class in van-colors

--- packages/tailwindcss-color-preset/colors/van-colors.js
+++ packages/tailwindcss-color-preset/colors/van-colors.js
@@ -19,6 +19,4 @@ export default {
     7: '#646566',
     8: '#323233',
   },
-  'van-gradient-red': 'linear-gradient(to right, #ff6034, #ee0a24)',
-  'van-gradient-orange': 'linear-gradient(to right, #ffd01e, #ff8917)',
 }
